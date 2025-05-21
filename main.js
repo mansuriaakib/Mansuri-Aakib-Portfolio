3d143427c774333ea184ada4be51467bb7331626
Mansuri Aakib <148616718+mansuriaakib@users.noreply.github.com>
2025-05-21 14:02:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ setTimeout(function () {
 }, 1500);
 
 // PAGE PRE-LOADER JS[END]
+
+
+// GET CONTACT FORM DATA WITH EMAIL JS
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault(); // Prevent page reload
+
+  emailjs.sendForm("service_yo81igk", "template_7m2kr4g", this)
+    .then(function () {
+      alert("Message sent successfully!");
+      document.getElementById("contact-form").reset(); // Optional: reset form
+    }, function (error) {
+      console.error("FAILED...", error);
+      alert("Failed to send message. Please try again.");
+    });
+});
